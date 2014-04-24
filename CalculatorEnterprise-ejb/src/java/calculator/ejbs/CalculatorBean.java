08641aadbb03d39674ad58945d1e108f36b1252e
Alessandro Stein <alessandrostein@github.com>
2014-04-23 21:40:04 -0300

    PApplication Enterprise

---
+++ CalculatorEnterprise-ejb/src/java/calculator/ejbs/CalculatorBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculator.ejbs;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author alessandro.stein
+ */
+@Stateless
+public class CalculatorBean implements CalculatorBeanLocal {
+
+    @Override
+    public double somar(double a, double b) {
+        return (a + b);
+    }
+}
