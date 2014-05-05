6a21bdee1416be05d436b9e1b697c78911adde8a
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-05 09:34:27 -0300

    List Role

--- CalculatorEnterprise-ejb/build/generated-sources/ap-source-output/calculator/ejbs/entity/Role_.java
+++ CalculatorEnterprise-ejb/build/generated-sources/ap-source-output/calculator/ejbs/entity/Role_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-05T08:06:05")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-05T09:32:26")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
