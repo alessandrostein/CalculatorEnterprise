c01f028313800b61917d002c154612772776d6c8
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-05 10:21:12 -0300

    Add Remove Servlet

--- CalculatorEnterprise-ejb/build/generated-sources/ap-source-output/calculator/ejbs/entity/Role_.java
+++ CalculatorEnterprise-ejb/build/generated-sources/ap-source-output/calculator/ejbs/entity/Role_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-05T09:58:46")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-05T10:19:27")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
